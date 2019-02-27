02edcf1239663fed0d60ebfbfc75e06c9ef0b4c9
Dusan Mihajlovic <dmihajlovic@itekako.com>
2019-02-27 15:15:02 +0100

    finished

--- src/app/pages/listViewPage.js
+++ src/app/pages/listViewPage.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import ListCard from './cards/listCard'
 import Header from '../partials/header'
 import Loader from './loader/loader'
+import Map from './mapViewPage'
 
 class ListViewPage extends Component {
     componentDidMount() {
@@ -23,6 +24,15 @@ class ListViewPage extends Component {
                     return <ListCard name={el.name} description={el.description} photo={el.photo}  key={el.id}/>
                 })}
             </div>}
+            <Map 
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key="
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            center={{ lat: -24.9923319, lng: 135.2252427 }}
+            zoom={2}
+            places={this.props.listOfOffices}
+            />
             </React.Fragment>
         )
     }
--- src/app/pages/mapViewPage.js
+++ src/app/pages/mapViewPage.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react'
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import React, {Component} from "react";
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  withScriptjs,
+  InfoWindow
+} from "react-google-maps";
+import {connect} from 'react-redux'
 
-const mapStyles = {
-    width: '100%',
-    height: '100%'
-  };
+class Map extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      listOfficesToMap : this.props.listOfOffices.length > 0  ? this.props.listOfOffices.map((el) => ({isOpen: true})) : []
+    }
+  }
 
-class MapViewPage extends Component {
+  openHandler(i){
+    this.setState({
+      listOfficesToMap: this.state.listOfficesToMap.map((el, j)=>{
+        if(i === j || el.isOpen) {
+          return {isOpen: true}
+        }else {
+          return {isOpen: false}
+        }
+      })
+    })
+  }
 
-    render() {
+  closeHandler(i) {
+    this.setState({
+      listOfficesToMap: this.state.listOfficesToMap.map((el, j)=>{
+          if(i === j || !el.isOpen) {
+            return {isOpen: false}
+          }else {
+            return {isOpen: true}
+          }
+      })
+    })
+  }
+  
+  render() {
+    return (
+    <GoogleMap defaultZoom={this.props.zoom} defaultCenter={this.props.center}  options={{ scrollwheel: true}}> 
+      {this.props.places.map((place, i) => {
         return (
-          <Map
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={{
-             lat: -1.2884,
-             lng: 36.8233
-            }}
-          />
+          <Marker
+            key={place.id}
+            position={{ lat: parseFloat(place.latitude), lng: parseFloat(place.longitude) }}
+            draggable={false}
+            onClick={() => {this.openHandler(i)}}
+          >
+            {this.state.listOfficesToMap.length > 0 ? this.state.listOfficesToMap[i].isOpen === true ? <InfoWindow onCloseClick={() => {this.closeHandler(i)}}>
+			        {place.photo !== null ? <img src={place.photo} className='grid-image-wrapper' alt='Not Found' /> : <p>Don't have image</p>}
+		        </InfoWindow> : null : null}
+          </Marker>
         );
-      }
-}
+      })}
+    </GoogleMap>
+  );
+    }
+};
 
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyDsHhS_Vdi_TzKiDyPlU0xZZONLKBEtbV0'
-  })(MapViewPage);
+const mapStateToProps = (state) => ({...state})
+
+
+export default connect(mapStateToProps)(withScriptjs(withGoogleMap(Map)));
